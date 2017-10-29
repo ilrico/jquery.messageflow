116405d08cecc5e72a3e6801687a5848138b77b1
rico_X1 <rico@syshedge.com>
2017-10-29 18:44:30 +0100

    cleaning js

--- jquery.messageflow.js
+++ jquery.messageflow.js
@@ -8,15 +8,12 @@ function MessageFlow() {
     }
     this.addMessage = function (type, msg, msg_id) {
         if(type) msgClass = 'messageflow_msg_' + type;
-        console.log(msg_id, msgClass);
-        if(typeof(msg_id) == 'undefined') console.log("no msg_id provided");
-        msg_id = 'msg_' + this.counter;
+        if(typeof(msg_id) == 'undefined') msg_id = 'msg_' + this.counter;
         var html_str = '<div id="' + msg_id + '" class="messageflow_msg ' + msgClass + '">' + msg + '</div>'
         $('#' + this.id).append(html_str);
         $('#' + msg_id).fadeOut(this.fadeoutDelay);
         setTimeout(function() {
             $('#' + msg_id).remove();
-            console.log(msg_id + ' removed');
         }, this.fadeoutDelay + 1000);
         this.counter += 1;
     }
